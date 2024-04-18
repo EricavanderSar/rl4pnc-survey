798d3c32c951bb8ddbabb2557e150e6d73a6805d
Erica van der Sar <ericavandersar@gmail.com>
2024-04-18 21:26:41 +1000

    Using params Blazej with old_env + param scaling & mod actionspace - adj RHO

--- train_ppo_baseline.sh
+++ train_ppo_baseline.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=rome
 #SBATCH --time=08:00:00
-#SBATCH --output=Case14_BlazejOldEnv_ppo_baseline_%j.out
+#SBATCH --output=Case14_BlazejRHOOldEnv_ppo_baseline_%j.out
 
 
 ENVNAME=rte_case14_realistic
