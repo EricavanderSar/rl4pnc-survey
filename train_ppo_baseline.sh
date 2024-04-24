6ca1d17230c361c6b71619c499cd3045e97d1574
Erica van der Sar <ericavandersar@gmail.com>
2024-04-24 10:31:55 +1000

    adjusted VF params for case 14 realistic

--- train_ppo_baseline.sh
+++ train_ppo_baseline.sh
@@ -6,10 +6,10 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=rome
 #SBATCH --time=24:00:00
-#SBATCH --output=Case14_SandboxMedha_ppo_baseline_%j.out
+#SBATCH --output=Case14_Real_ppo_baseline_%j.out
 
 
-ENVNAME=l2rpn_case14_sandbox
+ENVNAME=rte_case14_realistic #l2rpn_case14_sandbox
 WORKDIR=$TMPDIR/evds_output_dir
 RESDIR=Case14_Sandbox
 
