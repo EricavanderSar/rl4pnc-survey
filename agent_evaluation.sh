18c40b39ac888b635c169ebd595ca44169f29a7b
Erica van der Sar <ericavandersar@gmail.com>
2024-11-26 12:06:51 +0100

    Creating visualizations etc in Agent_evaluation.ipynb

--- agent_evaluation.sh
+++ agent_evaluation.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=rome
-#SBATCH --time=16:00:00
-#SBATCH --output=Eval_Agent_%j.out
+#SBATCH --time=08:00:00
+#SBATCH --output=Eval_Agent_Gr_n-1_%j.out
 
 AGENT_TYPE="heur" # options "heur" or "rl"
 RESDIR=$HOME/HeuristicBaselines/
@@ -19,5 +19,6 @@ export PYTHONPATH=$PYTHONPATH:$PWD
 
 j=${SLURM_JOB_ID}
 echo "Run code:"
-time srun python -u scripts/agent_evaluation.py -a $AGENT_TYPE -c $CHRONICS -p $RESDIR -l $LIBDIR -at 0.95 -j $j #-o -lr -ld -rt 0.8 -s
+time srun python -u scripts/agent_evaluation.py -a $AGENT_TYPE -c $CHRONICS -p $RESDIR -l $LIBDIR -at 0.95 -j $j
+ #-o -lr -ld -rt 0.8 -s
 echo "Done"
