75f17260919f9db4f41b47bc7c63fcdcfa7a0689
Erica van der Sar <ericavandersar@gmail.com>
2024-12-05 16:13:58 +0100

    Adjust job agent_evaluation to get summarized data

--- agent_evaluation.sh
+++ agent_evaluation.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --partition=rome
 #SBATCH --time=08:00:00
-#SBATCH --output=Evaluation_Results_Case14/Eval_Agent_Gr_n-1_%j.out
+#SBATCH --output=Evaluation_Results_Case14/Summarize_Eval_Agents_%j.out
 
 AGENT_TYPE="heur" # options "heur" or "rl"
 RESDIR=$HOME/HeuristicBaselines/
@@ -17,8 +17,13 @@ echo "Activate envirnonment"
 source activate rl4pnc
 export PYTHONPATH=$PYTHONPATH:$PWD
 
-j=${SLURM_JOB_ID}
-echo "Run code:"
-time srun python -u scripts/agent_evaluation.py -a $AGENT_TYPE -c $CHRONICS -p $RESDIR -l $LIBDIR -at 0.95 -j $j -o -ld
-#-o -lr -ld -rt 0.8 -s
+#j=${SLURM_JOB_ID}
+#echo "Run agent_evaluation.py..."
+#time srun python -u scripts/agent_evaluation.py -a $AGENT_TYPE -c $CHRONICS -p $RESDIR -l $LIBDIR -at 0.95 -j $j -o -ld
+##-o -lr -ld -rt 0.8 -s
+#echo "Done"
+
+echo "Run summarize_evaluation_data.py..."
+time srun python -u scripts/summarize_evaluation_data.py -p $RESDIR -c $CASE -m $MAXSTEPS
 echo "Done"
+
