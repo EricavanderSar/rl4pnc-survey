71a0aceb25596356f88657259819234c84525aba
Erica van der Sar <ericavandersar@gmail.com>
2024-12-11 15:06:06 +0100

    Retest PPO baseline without value function clipping param (default = 10)

--- train_ppo_baseline.sh
+++ train_ppo_baseline.sh
@@ -12,7 +12,7 @@
 
 ENVNAME=l2rpn_case14_sandbox #rte_case14_realistic #l2rpn_icaps_2021_large #
 WORKDIR=$TMPDIR/evds_output_dir
-RESDIR=Case14_SurveyPaper
+RESDIR=Case14_SurveyPaper2
 
 # function to handle the SIGTERM signal
 function handle_interrupt {
