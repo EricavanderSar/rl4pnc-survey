76ddc40af13831ae9231a8c27bb0584d1b6b7307
Erica van der Sar <ericavandersar@gmail.com>
2025-01-28 12:17:52 +0100

    New: Line reconnect and disconnect option during training (jobscript update)

--- train_ppo_baseline.sh
+++ train_ppo_baseline.sh
@@ -5,14 +5,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=rome
-#SBATCH --time=16:00:00
+#SBATCH --time=20:00:00
 #SBATCH --output=Train_Results_Case14/Case14_Baseline_ppo_%j.out
 #SBATCH --array=1-5
 #SBATCH --ear=off
 
 ENVNAME=l2rpn_case14_sandbox #rte_case14_realistic #l2rpn_icaps_2021_large #
 WORKDIR=$TMPDIR/evds_output_dir
-RESDIR=Case14_SurveyPaperAT
+RESDIR=Case14_SurveyPaperLine
 
 # function to handle the SIGTERM signal
 function handle_interrupt {
