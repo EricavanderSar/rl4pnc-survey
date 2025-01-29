bbdbdb923d6524552783473f51b3b9a672c10053
Erica van der Sar <ericavandersar@gmail.com>
2025-01-29 14:56:29 +0100

    Setup for reset topo experiments

--- train_ppo_baseline.sh
+++ train_ppo_baseline.sh
@@ -12,7 +12,7 @@
 
 ENVNAME=l2rpn_case14_sandbox #rte_case14_realistic #l2rpn_icaps_2021_large #
 WORKDIR=$TMPDIR/evds_output_dir
-RESDIR=Case14_SurveyPaperLine
+RESDIR=Case14_SurveyPaperReset
 
 # function to handle the SIGTERM signal
 function handle_interrupt {
